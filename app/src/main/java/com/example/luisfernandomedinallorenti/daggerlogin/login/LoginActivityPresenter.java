2b488b9ccb8f527280559d6d3e7b01ff5fd69193
lfmll <luisfernandomedinallorenti@gmail.com>
2020-01-12 23:35:54 -0400

    implementacion Presentador

--- app/src/main/java/com/example/luisfernandomedinallorenti/daggerlogin/login/LoginActivityPresenter.java
+++ app/src/main/java/com/example/luisfernandomedinallorenti/daggerlogin/login/LoginActivityPresenter.java
@@ -1,4 +1,45 @@
 package com.example.luisfernandomedinallorenti.daggerlogin.login;
 
-public class LoginActivityPresenter {
+import android.support.annotation.Nullable;
+
+public class LoginActivityPresenter implements LoginActivityMVP.Presenter {
+    @Nullable
+    private LoginActivityMVP.View view;
+    private LoginActivityMVP.Model model;
+
+    public LoginActivityPresenter(LoginActivityMVP.Model model){
+        this.model=model;
+    }
+
+    @Override
+    public void setView(LoginActivityMVP.View view) {
+        this.view=view;
+    }
+
+    @Override
+    public void loginButttonCLicked() {
+        if (view != null){
+            if (view.getFirstName().trim().equals("")|| view.getLastName().trim().equals("")){
+                view.showInputError();
+            }else {
+                model.createUser(view.getFirstName().trim(),view.getLastName().trim());
+                view.showUserSaved();
+            }
+        }
+    }
+
+    @Override
+    public void getCurrentUser() {
+        User user=model.getUser();
+        if (user==null){
+            if (view!=null){
+                view.showUserNotAvailable();
+            }
+        }else {
+            if (view!=null){
+                view.setFirstName(user.getFirstName());
+                view.setLastName(user.getLastName());
+            }
+        }
+    }
 }
